0c5e954a487c9619e8f72d585affbfbcaf00c96c
Andrey Kovalenko <ak@taucode.com>
2019-12-22 18:51:53 +0200

    Migrated.

---
+++ src/TauCode.Cqrs.Testing/CommandValidatorTestBase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using FluentValidation;
+using FluentValidation.Results;
+using NUnit.Framework;
+using TauCode.Cqrs.Commands;
+
+namespace TauCode.Cqrs.Testing
+{
+    [TestFixture]
+    public abstract class CommandValidatorTestBase<TCommand, TValidator>
+        where TCommand : ICommand
+        where TValidator : AbstractValidator<TCommand>
+    {
+        protected TValidator Validator { get; set; }
+
+        [SetUp]
+        public void SetUpBase()
+        {
+            this.Validator = this.CreateValidator();
+        }
+
+        protected ValidationResult ValidateCommand(TCommand command)
+        {
+            var validationResult = this.Validator.Validate(command);
+            return validationResult;
+        }
+
+        protected virtual TValidator CreateValidator()
+        {
+            var ctor = typeof(TValidator).GetConstructor(Type.EmptyTypes);
+            if (ctor == null)
+            {
+                throw new InvalidOperationException($"No parameterless constructor defined for type '{typeof(TValidator).FullName}'.");
+            }
+
+            var validator = (TValidator) ctor.Invoke(new object[0]);
+            return validator;
+        }
+
+        protected abstract TCommand CreateCommand();
+    }
+}
---
+++ src/TauCode.Cqrs.Testing/QueryValidatorTestBase.cs
@@ -0,0 +1,42 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using NUnit.Framework;
+using System;
+using TauCode.Cqrs.Queries;
+
+namespace TauCode.Cqrs.Testing
+{
+    [TestFixture]
+    public abstract class QueryValidatorTestBase<TQuery, TValidator>
+        where TQuery : IQuery
+        where TValidator : AbstractValidator<TQuery>
+    {
+        protected TValidator Validator { get; set; }
+
+        [SetUp]
+        public void SetUpBase()
+        {
+            this.Validator = this.CreateValidator();
+        }
+
+        protected ValidationResult ValidateQuery(TQuery query)
+        {
+            var validationResult = this.Validator.Validate(query);
+            return validationResult;
+        }
+
+        protected virtual TValidator CreateValidator()
+        {
+            var ctor = typeof(TValidator).GetConstructor(Type.EmptyTypes);
+            if (ctor == null)
+            {
+                throw new InvalidOperationException($"No parameterless constructor defined for type '{typeof(TValidator).FullName}'.");
+            }
+
+            var validator = (TValidator) ctor.Invoke(new object[0]);
+            return validator;
+        }
+
+        protected abstract TQuery CreateQuery();
+    }
+}
